e5e1937f8f71675b0f368bee3300bfe52b1ecdd4
filiptrajkovik <filiptrajkovik13@gmail.com>
2025-05-17 12:41:38 +0200

    Initial commit

---
+++ src/main/java/org/example/inventorymanagementbackend/InventoryManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.inventorymanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryManagementBackendApplication.class, args);
+    }
+
+}
